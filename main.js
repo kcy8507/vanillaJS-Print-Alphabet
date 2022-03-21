a174c2261bf98e5e83e628b587deb70297b45e10
chaeyeon kim <kcy85078507@gmail.com>
2022-03-21 16:24:22 +0900

    update [...x];

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
-function popAlphabet(x) {
-  const arr = x.split(""); //x를 한 글자씩 쪼개줬다.
-  const numArr = [];
+function printAlphabet(x) {
+  let alphabet = [...x];
+  console.log(alphabet);
+  console.log(`입력한 배열: ${alphabet}`);
 
-  console.log(`입력한 배열: ${arr}`);
-
-  arr.forEach((el, i) => {
-    let count = "";
-    if (numArr.includes(arr[i])) {
-      count++;
-      numArr.push(`${count}개`);
-      //   console.log
-    } else {
-      count++;
-      console.log(count);
-      numArr.push(`${arr[i]}: ${count}개`);
-    }
-  });
-  console.log(numArr);
+  alphabet.forEach((el, i) => {});
+  console.log(alphabet);
 }
-popAlphabet("aaer");
+printAlphabet("aabc");

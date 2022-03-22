965c662d2b9fc09121059ed8be80387325224624
chaeyeon kim <kcy85078507@gmail.com>
2022-03-22 11:51:19 +0900

    Update forEach

--- main.js
+++ main.js
@@ -1,27 +1,10 @@
-// function printAlphabet(x) {
-//   let alphabet = [...x]; //alphabet이라는 배열에 넣었음.
-//   let print = {}; //print라는 새로운 객체를 만들어줬다.
-//   console.log(`입력한 배열: ${alphabet}`);
-
-//   let count = ""; // 갯수를 세기 위해 count 변수를 만들어주고 반복문 전에 초기화 해줌.
-//   alphabet.forEach((el, i) => {
-//     if ("a" in print) {
-//       print = count++;
-//       console.log(print, count);
-//     } else {
-//       print.push(count);
-//     }
-//   });
-//   console.log("결과", print);
-// }
-// printAlphabet("aabc");
-
-// let alphabet = ["a", "b", "c", "a"];
-// let print = alphabet.reduce((a, x) => {
-//   a[x] = (a[x] || 0) + 1;
+// const words = ["Beachball", "Rodeo", "Angel", "Aardvark", "Xylophone", "November", "Chocolate", "Papaya", "Uniform", "Joker", "Clover", "Bali"];
+// const alphabetical = words.reduce((a, x) => {
+//   if (!a[x[0]]) a[x[0]] = [];
+//   a[x[0]].push(x);
 //   return a;
 // }, {});
-// console.log(print);
+// console.log(alphabetical);
 
 function result(x) {
   let alphabet = [...x];
@@ -32,11 +15,21 @@ function result(x) {
   console.log(print);
 }
 result("abbbes");
+// reduce 이용
 
-// const words = ["Beachball", "Rodeo", "Angel", "Aardvark", "Xylophone", "November", "Chocolate", "Papaya", "Uniform", "Joker", "Clover", "Bali"];
-// const alphabetical = words.reduce((a, x) => {
-//   if (!a[x[0]]) a[x[0]] = [];
-//   a[x[0]].push(x);
-//   return a;
-// }, {});
-// console.log(alphabetical);
+function countTxt(str) {
+  let result = {};
+  [...str].forEach(function (data) {
+    // console.log(data);
+    if (!result[data]) {
+      // 객체에 data 키값이 존재하나요?
+      result[data] = 1;
+    } else {
+      // result[data] = result[data] + 1;
+      result[data]++;
+    }
+  });
+  return result;
+}
+console.log(countTxt("aabbcc"));
+// forEach 이용
